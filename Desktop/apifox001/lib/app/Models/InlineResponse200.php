f4dca7bade037efb2f5bdd5c7cd5112486636b7e
30740790 <30740790@qq.com>
2023-02-07 13:56:03 +0800

    提交lib

---
+++ Desktop/apifox001/lib/app/Models/InlineResponse200.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * InlineResponse200
+ */
+namespace app\Models;
+
+/**
+ * InlineResponse200
+ */
+class InlineResponse200 {
+
+    /** @var int $errCode */
+    private $errCode;
+
+    /** @var string $dataList */
+    private $dataList;
+
+}
